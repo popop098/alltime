137225ca3cd676ea97cf6595c60b1060183af7e3
popop098 <popop0982@naver.com>
2022-01-12 13:20:43 +0900

    .

--- next.config.js
+++ next.config.js
@@ -3,7 +3,4 @@ module.exports = {
   images: {
     domains: ['cdn.discordapp.com'],
   },
-  environment:{
-    MONGODB_URI : 'mongodb+srv://alltime:txnWME6zmfHvSVL@cluster0.mp1qa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-  }
 };
